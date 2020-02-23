dfc32f133ee36616eabfbd3499f7c7281059f2b2
Chris Ward <veggie2u@cyberward.net>
2020-02-23 16:48:15 -0600

    Add RoboRio and hardware info

---
+++ NoMythic_SikLights/NoMythic_SikLights.ino
@@ -0,0 +1,168 @@
+/*******************************************************
+ * NoMythic SikLights Neopixels controlled over I2C    *
+ * Listen on I2C for light animation commands.         *
+ *******************************************************/
+
+#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+/*******************************************************
+ * NeoPixel Config                                     *
+ *******************************************************/
+ 
+// The pin the NeoPixels are attached to
+// These are analog input pins, but are used due to location on the board
+#define PIN1       14
+#define PIN2       15
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS  120
+
+// How bright are the neo pixels? (0-100)
+#define BRIGHTNESS 100
+
+// Delay defines the blink rate in ms
+#define DELAY      400
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+Adafruit_NeoPixel pixels1 = Adafruit_NeoPixel(NUMPIXELS, PIN1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels2 = Adafruit_NeoPixel(NUMPIXELS, PIN2, NEO_GRB + NEO_KHZ800);
+
+// Testing different purples
+// pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+uint32_t purple = pixels1.Color(102, 000, 204); // Purple
+uint32_t color2 = pixels1.Color(161, 000, 255);
+uint32_t color3 = pixels1.Color(150, 025, 148);
+uint32_t color4 = pixels1.Color(91, 36, 98);
+uint32_t orange = pixels1.Color(255, 69, 0);
+uint32_t red = pixels1.Color(255, 0, 0);
+
+/*******************************************************
+ * I2C Config                                          *
+ *******************************************************/
+
+//The I2C device number. Must use same number in sender
+const byte DEVICE_NO = 4;
+// Status pin - a led - onboard is 13 - pulses for each command
+const byte STATUS_PIN = 13;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("NoMythic SikLights");
+  pinMode(13, OUTPUT);
+
+  // Strand 1
+  pixels1.begin(); // This initializes the NeoPixel library.
+  pixels1.setBrightness(BRIGHTNESS);
+
+  // Strand 2
+  pixels2.begin(); // This initializes the NeoPixel library.
+  pixels2.setBrightness(BRIGHTNESS);
+
+  Wire.begin(DEVICE_NO);
+  // when an I2C event happens, process it
+  Wire.onReceive(handleEvent);
+  Serial.println("I2C Ready");
+
+  setSolidColor1(purple);
+  setSolidColor2(purple);
+
+  Serial.println("Pixels On");
+}
+
+/*******************************************************
+ * Every time we hear an I2C event for our device,     *
+ * handle the command here                             *
+ *******************************************************/
+void handleEvent(int howMany) {
+  digitalWrite(STATUS_PIN, HIGH);
+  while(Wire.available()) {
+    byte command = Wire.read();
+    Serial.print("Command: ");
+    Serial.println(command);
+    // we have predefined commands that both send and receive understand
+    switch(command) {
+      case 1: // turn off command
+        turnOff1();
+        turnOff2();
+        break;
+      case 2: // turn on command
+        setSolidColor1(purple);
+        setSolidColor2(purple);
+        break;
+      case 3: // last 30 sec command
+        last30Seconds1();
+        last30Seconds2();
+        break;
+      case 4: // limelight lined up
+        blinkRed();
+    }
+  }
+  digitalWrite(STATUS_PIN, LOW);
+}
+
+// Nothing happening right now
+void loop() {
+  delay(DELAY);
+}
+
+// Set all led on strand 1 to one color
+void setSolidColor1(uint32_t color) {
+  for(int i=0;i<NUMPIXELS;i++){
+    pixels1.setPixelColor(i, color);
+  }
+  pixels1.show();
+}
+
+// Set all led on strand 2 to one color
+void setSolidColor2(uint32_t color) {
+  for(int i=0;i<NUMPIXELS;i++){
+    pixels2.setPixelColor(i, color);
+  }
+  pixels2.show();
+}
+
+// Turn off all lights on strand 1
+void turnOff1() {
+  for(int i=0;i<NUMPIXELS;i++){
+    pixels1.setPixelColor(i, 0);
+  }
+  pixels1.show();
+}
+
+// Turn off all lights on strand 2
+void turnOff2() {
+  for(int i=0;i<NUMPIXELS;i++){
+    pixels2.setPixelColor(i, 0);
+  }
+  pixels2.show();
+}
+
+void last30Seconds1() {
+  int start = NUMPIXELS / 3;
+  int finish = start * 2;
+  for(int i = start;i < finish;i++){
+    pixels1.setPixelColor(i, orange);
+  }
+  pixels1.show();
+}
+
+void last30Seconds2() {
+  int start = NUMPIXELS / 3;
+  int finish = start * 2;
+  for(int i = start;i < finish;i++){
+    pixels2.setPixelColor(i, orange);
+  }
+  pixels2.show();
+}
+
+void blinkRed() {
+  setSolidColor1(red);
+  setSolidColor2(red);
+  delay(300);
+  // setPurple();
+}
--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # 2019-SikLights
-A way to control strands of NeoPixels over I2C
+
+Siklights are lights of some kind on an FRC robot.
+
+## What does this do?
+
+This Arduino program recieves commands over i2c and then controls NeoPixels strips based on the commands recieved.
+The commands are send from the RoboRio code over i2c.
+
+## What for?
+
+Our goal was 2 fold.
+1. To provide some interest (sick lights) on the robot
+2. To provide some visual feedback to operators
+
+## How does the Arduino code work?
+
+This code uses 2 libraries, Wire, to communicate over i2c, and Adafruit_NeoPixel to control a strand of led lights.
+
+This is just the reciever.
+
+The code is using a few pins.
+- 4 & 5 are the i2c pins on an Ardunino UNO. Check your board.
+- 14 & 15 are being used to control the NeoPixels (we had 2 strands)
+- 13 is the built in Arduino UNO status light.
+
+The code initializes both the Wire library and the NeoPixels in setup. The real magic is in the handleEvent method.
+
+The handleEvent method is called when ever a command is recieved by i2c. The commands are simply numbers. This requires the sender program and this reciever program to jointly decide what these command numbers mean. There are more descriptive ways to do this, but this was done for simplicity. Only one byte needed.
+
+When a command is recieved, methods to change the colors on the lights are called. There is one call per strand. This is code that should change the state of a strip of lights and end. This system can not be used to run animations.
+
+## How does the RoboRio code work?
+
+This is not the most elegant code, but it did work. Keep in mind this was 2019 code, and some locations you might put this code for 2020 are different.
+
+In 2019's Robot class you would need to define some variables like this:
+```
+	// I2C Variables
+  private I2C.Port i2cPort;
+  private byte[] dataSend;
+  private byte[] dataRecieve;
+  private I2C i2c;
+  private int deviceNo;
+```
+Then you would need a setup method that could be called from robotInit() like this:
+```
+private void doI2cSetup() {
+    i2cPort = I2C.Port.kMXP; // when using the MXP expansion Port
+    // i2cPort = I2C.Port.kOnboard; // when using the RoboRio built in port
+    deviceNo = 4; // needs to match Arduino
+    i2c = new I2C(i2cPort, deviceNo);
+    dataSend = new byte[1];
+    dataRecieve = new byte[1];
+    System.out.println("Init I2C");
+
+    dataSend[0] = 2;
+    i2c.transaction(dataSend, 1, dataRecieve, 1);
+    System.out.println("Turn purple lights on");
+  }
+```
+Then you can send a command to the Arduino. These commands are just a single byte. We only create a few of these. You can see more of what the commands did if you look at the Arduino code.
+
+We used this code in teleopPeriodic() to turn warning lights on at 30sec to go:
+
+```
+public void teleopPeriodic() {
+    Scheduler.getInstance().run();
+    double timeLeft = DriverStation.getInstance().getMatchTime();
+    // if less than 30 sec run once
+    if (timeLeft <= 30 && is30Sec == false && timeLeft != -1) {
+      dataSend[0] = 3;
+      i2c.transaction(dataSend, 1, dataRecieve, 1);
+      System.out.println("Turn on 30sec animation");
+      is30Sec = true;
+    }
+  }
+```
+The key part is just setting the command you want `dataSend[0] = 3` and then sending it.
+
+## How did the lights get hooked up?
+
+We were using a RioDuino http://www.revrobotics.com/rev-11-1104/ that is now discontinued that plugged into the MPX port. You can use the I2C pins on the MPX port or the seperate I2C port on the RoboRio. Just make sure to specify which one you are using in the Arduino code. You can then pick any two output pins on the RioDuino or other microcontroler to use as the NeoPixel output pins. The NeoPixels also need power and ground. BUT, the output from the RoboRio, or a microcontroller is not enough to power a hundred or more leds. You will need to connect another 5v power source on the robot that will have enough juice. Don't power your microcontroller with this, just the leds, and tie the grounds together.
